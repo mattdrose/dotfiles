60e4f70fbdce3db60614df7047773c36e9082186
Matt Rose <rose.matt@gmail.com>
2015-07-20 16:38:27 -0400

    Add nvm

--- npm.sh
+++ npm.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 
-# Install common npm tools
-
 # Ask for the administrator password upfront.
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Upgrade existing packages.
-npm update
+# Install nvm
+brew install nvm
+
+mkdir ~/.nvm
+
+cp $(brew --prefix nvm)/nvm-exec ~/.nvm/
+
+# Source nvm
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+
+#Install latest
+nvm install
 
 # Install Grunt CLI
 npm install -g grunt-cli
+
+# Install Gulp
+npm install -g gulp
 
 # Install Bower
 npm install -g bower
